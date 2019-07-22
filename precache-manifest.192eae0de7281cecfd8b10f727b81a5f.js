92779ee701ab153e2d74c68ac6f4c3b2a55f24d4
ForexXard <forex.xard@gmail.com>
2019-07-22 18:16:32 -0400

    Updates

---
+++ precache-manifest.192eae0de7281cecfd8b10f727b81a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e90ea1b00347204ead947b478026b75",
+    "url": "/Monster/index.html"
+  },
+  {
+    "revision": "4d5ddf7c1aba23ec5be2",
+    "url": "/Monster/static/css/main.fd6e27e0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monster/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4d5ddf7c1aba23ec5be2",
+    "url": "/Monster/static/js/main.65609aaa.chunk.js"
+  },
+  {
+    "revision": "cded30659477d4f2a55b",
+    "url": "/Monster/static/js/runtime~main.09aeed93.js"
+  }
+]);
